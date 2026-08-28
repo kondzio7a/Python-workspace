59ab6d3470e3995e379b9f39dc4df661e26cc5d1
Konrad Angelo <konradangelo@gmail.com>
2026-08-28 23:06:16 +0200

    def check_rescources

--- OOP - Coffee machine/coffee_maker.py
+++ OOP - Coffee machine/coffee_maker.py
@@ -10,4 +10,10 @@ class CoffeeMaker(object):
         print(f"Capacity of coffee {self.resources.get('coffee')}")
         print(f"Capacity of milk{self.resources.get('milk')}")
 
-    
+    def check_resource(self, coffee):
+        make_coffee = True
+        for item in coffee.ingredients:
+            if coffee.ingredients[item] > self.resources.get(item):
+                print(f"Sorry, {item} is not available, please add")
+                make_coffee = False
+        return make_coffee
